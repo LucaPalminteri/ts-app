1feeaab061be99090cfe3324e66a29199cd0cdbd
LucaPalminteri <lucapalminteri02@gmail.com>
2022-12-12 20:58:48 -0300

    add title screen one

--- screens/TabOneScreen.tsx
+++ screens/TabOneScreen.tsx
@@ -8,6 +8,8 @@ export default function TabOneScreen({ navigation }: RootTabScreenProps<'TabOne'
   return (
     <View style={styles.container}>
       <Text style={styles.title}>Tab One</Text>
+      <Text>Luca Palminteri App</Text>
+      <Text>TypeScript</Text>
       <View style={styles.separator} lightColor="#eee" darkColor="rgba(255,255,255,0.1)" />
       <EditScreenInfo path="/screens/TabOneScreen.tsx" />
     </View>
