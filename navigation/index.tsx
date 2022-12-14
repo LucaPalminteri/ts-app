7400f12920f1cb32ff67ae042d493900b5bc6342
LucaPalminteri <lucapalminteri02@gmail.com>
2022-12-14 19:33:03 -0300

    delete import unused

--- navigation/index.tsx
+++ navigation/index.tsx
@@ -2,7 +2,6 @@ import { FontAwesome } from '@expo/vector-icons';
 import { createBottomTabNavigator } from '@react-navigation/bottom-tabs';
 import { NavigationContainer, DefaultTheme, DarkTheme } from '@react-navigation/native';
 import { createNativeStackNavigator } from '@react-navigation/native-stack';
-import * as React from 'react';
 import { ColorSchemeName, Pressable } from 'react-native';
 
 import Colors from '../constants/Colors';
